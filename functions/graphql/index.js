c0253b7d4f9f8474f0ac47eaab55ae6e904af236
Arslan-Ali-Ch <105070803+Arslan-Ali-Ch@users.noreply.github.com>
2022-07-21 02:02:44 +0500

    zx xcxzx

--- functions/graphql/index.js
+++ functions/graphql/index.js
@@ -25,10 +25,10 @@ const resolvers = {
   Query: {
     todos:async (parent,args,{user})=>{
       console.log('i am user',user);
-      if(!user){
-        return [];
-      }
-      else{
+      // if(!user){
+      //   return [];
+      // }
+      
         const results= await client.query(
           q.Paginate(q.Match(q.Index("todos_by_user"),user))
         );
@@ -38,15 +38,15 @@ const resolvers = {
           done
         }));
     
-      }
+      
     }  
   
   },
   Mutation:{
     addTodo:async (_,{text},{user})=>{
-      if(!user){
-        throw new Error("Must be authenticated");
-      }
+      // if(!user){
+      //   throw new Error("Must be authenticated");
+      // }
       const results=await client.query(
         q.Create(q.Collection("todos"),{
           data:{
@@ -63,12 +63,12 @@ const resolvers = {
 
     },
 
-  updateTodoDone:async(_,{id},{user},{done})=>{
+  updateTodoDone:async(_,{id},{done})=>{
     
  
-    if(!user){
-      throw new Error("Must be authenticated");
-    }
+    // if(!user){
+    //   throw new Error("Must be authenticated");
+    // }
     const results=await client.query(
       q.Update(q.Ref(q.Collection("todos"),id),{
         data:{done:done
@@ -91,15 +91,15 @@ const resolvers = {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context:({context})=>{
-      if(context.clientContext.user){
-        return {user:context.clientContext.user.sub};
+    // context:({context})=>{
+    //   if(context.clientContext.user){
+    //     return {user:context.clientContext.user.sub};
     
-      }
-      else {
-        return {};
-      }
-    },
+    //   }
+    //   else {
+    //     return {};
+    //   }
+    // },
     playground:true,
     introspection:true,
   });
